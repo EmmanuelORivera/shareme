2f0ff93cb511f3ed8be43acd3dcb7e08e1f1a10e
Emmanuel Ochoa <eorivera95@gmail.com>
2023-02-20 15:04:18 -0600

    feat: navigate to /login when user is not on the localstorage

--- shareme_frontend/src/App.jsx
+++ shareme_frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './components/Login'
 import Home from './container/Home'
+import { fetchUser } from './utils/fetchUser'
 
 function App() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const user = fetchUser()
+    if (!user) navigate('/login')
+  }, [])
+
   return (
     <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_API_TOKEN}>
       <Routes>
